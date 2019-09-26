838d4940136b9e191818a9eae025e468dad9fcd0
Favour Ori <orifavour2000@gmail.com>
2019-09-26 20:48:12 +0200

    minor change

--- app.js
+++ app.js
@@ -5,17 +5,24 @@ const socket = require('socket.io')
 app.use(express.static('public'))
 
 
-
 //spin up the server
 let port = process.env.PORT || 3000
 let server = app.listen(port, () => {
     console.log("Server is up & Running..")
 })
 
+
 //socket setup
 const io = socket(server)
 io.on("connection", (socket) => {
-    console.log("Made socket connection")
+    console.log("Made socket connection", socket.id)
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
 })
 
 
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,32 @@
 //socket connection
 
 const socket = io.connect('http://localhost:3000/')
+
+//dom
+
+const message = document.getElementById('message')
+const handle = document.getElementById('handle')
+const output = document.getElementById('output')
+const btn = document.getElementById('send')
+const feedback = document.getElementById('feedback')
+
+//emit events
+
+btn.addEventListener('click', () => {
+    socket.emit('chat', { message: message.value, handle: handle.value })
+})
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value)
+})
+
+//listen for events
+
+socket.on('chat', (data) => {
+    feedback.innerHTML = ""
+    output.innerHTML += `<p><strong> ${data.handle}:</strong> ${data.message}</p> `
+})
+
+socket.on('typing', (data) => {
+feedback.innerHTML = `${data} is typing..`
+})
